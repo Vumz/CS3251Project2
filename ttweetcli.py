284a58d11f31555ab15624e18617f093df89be9b
Will Hunter <jhunter64@gatech.edu>
2020-04-04 22:08:48 -0400

    subscribe and unsubscribe done (to be tested more later)

--- ttweetcli.py
+++ ttweetcli.py
@@ -1,29 +1,36 @@
 import socket
 import sys
 
-HASHTAGS = []
-
-def subscribe(hashtag):
-  if len(HASHTAGS) >= 3:
-    print('operation failed: sub',  hashtag, 'failed, already exists or exceeds 3 limitation')
-  else:
-    print("subscribing", hashtag)
-    HASHTAGS.append(hashtag)
-
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 USERNAME = sys.argv[3]
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
 client.sendall(bytes(USERNAME,'UTF-8'))
+
 while True:
-  server_data =  client.recv(1024)
-  print("(server)", server_data.decode())
-  user_input = input()
-  client.sendall(bytes(user_input, 'UTF-8'))
-  if user_input == 'subscribe':
-    subscribe('test')
-  if user_input == 'exit':
-    break
+    server_data =  client.recv(1024)
+    print("(server)", server_data.decode())
+    user_input = input()
+    if 'unsubscribe' in user_input:
+        if '#' in user_input:
+            start = user_input.index('#')
+            hashtag = user_input[start:]
+            client_message = 'unsubscribe,' + USERNAME + ',' + hashtag
+            client.sendall(bytes(client_message, 'UTF-8'))
+        else:
+            client.sendall(bytes('none', 'UTF-8'))
+    elif 'subscribe' in user_input:
+        if '#' in user_input:
+            start = user_input.index('#')
+            hashtag = user_input[start:]
+            client_message = 'subscribe,' + USERNAME + ',' + hashtag
+            client.sendall(bytes(client_message, 'UTF-8'))
+        else:
+            client.sendall(bytes('none', 'UTF-8'))
+    if user_input == 'exit':
+        client.sendall(bytes(user_input, 'UTF-8'))
+        break
+    else:
+        client.sendall(bytes('none', 'UTF-8'))
 client.close()
-print(HASHTAGS)
--- ttweetser.py
+++ ttweetser.py
@@ -2,29 +2,57 @@ import socket, threading
 import sys
 
 TIMELINE = {
-    '#tag1': 'tweet1'
+    '#tag1': ['tweet1']
 }
-HASHTAGS = []
+HASHTAGS = {
+    'will': ['#tag1']
+}
+
+def subscribe(username, hashtag):
+    if len(HASHTAGS[username]) >= 3 or hashtag in HASHTAGS[username]:
+        print('operation failed: sub',  hashtag, 'failed, already exists or exceeds 3 limitation')
+    else:
+        print("subscribing", hashtag)
+        HASHTAGS[username].append(hashtag)
+        print(HASHTAGS)
+
+def unsubscribe(username, hashtag):
+    if hashtag in HASHTAGS[username]:
+        print("unsubscribing", hashtag)
+        HASHTAGS[username].remove(hashtag)
+        print(HASHTAGS)
+
 
 class ClientThread(threading.Thread):
     def __init__(self, clientAddress, clientSocket):
         threading.Thread.__init__(self)
         self.clientSocket = clientSocket
-        print ("New connection added: ", clientAddress)
+        print("New connection added: ", clientAddress)
     def run(self):
-        print ("Connection from : ", clientAddress)
-        #self.clientSocket.send(bytes("Hi, This is from Server..",'utf-8'))
+        print("Connection from : ", clientAddress)
         clientMessage = ''
         while True:
             data = self.clientSocket.recv(2048)
             clientMessage = data.decode()
-            if clientMessage == 'subscribe':
+            if 'unsubscribe' in clientMessage:
+                print('server unsubscribing')
+                name_index = clientMessage.index(',') + 1
+                tag_index = clientMessage.index('#')
+                username = clientMessage[name_index:tag_index - 1]
+                hashtag = clientMessage[tag_index:]
+                unsubscribe(username, hashtag)
+            elif 'subscribe' in clientMessage:
                 print('server subscribing')
+                name_index = clientMessage.index(',') + 1
+                tag_index = clientMessage.index('#')
+                username = clientMessage[name_index:tag_index - 1]
+                hashtag = clientMessage[tag_index:]
+                subscribe(username, hashtag)
             if clientMessage == 'exit':
-              break
-            print ("(client)", clientMessage)
+                break
+            print("(client)", clientMessage)
             self.clientSocket.send(bytes(clientMessage, 'UTF-8'))
-        print ("Client at ", clientAddress, " disconnected...")
+        print("Client at ", clientAddress, " disconnected...")
 LOCALHOST = "127.0.0.1"
 PORT = int(sys.argv[1])
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
